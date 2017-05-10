1baf44d44cd3011815f25e16d2ef0d9077443add
CryptoCoinUser <adkantor@gmail.com>
2017-05-09 19:02:24 -0700

    moved DATABASE_URL into .env

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://avram:izypizy@ds127101.mlab.com:27101/blog2';
+
 exports.TEMP_DATABASE_URL = (
 	process.env.TEMP_DATABASE_URL ||
 	'mongodb://localhost/blog-chai');
-exports.PORT = process.env.PORT || 3030;
+exports.PORT = process.env.PORT || 3030; 
