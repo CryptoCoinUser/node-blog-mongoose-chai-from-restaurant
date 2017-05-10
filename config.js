4dd8fe5205e990dad231a082464a006070b8397e
CryptoCoinUser <adkantor@gmail.com>
2017-05-10 14:14:23 -0700

    localhost while mLab is in .env

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
-
-
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/blog-chai';
-
+                      'mongodb://localhost/blog2';
 
 
 exports.TEMP_DATABASE_URL = (
 	process.env.TEMP_DATABASE_URL ||
-	'mongodb://localhost/blog-chai');
+	'mongodb://localhost/blog2');
 exports.PORT = process.env.PORT || 3030; 
